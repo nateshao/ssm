781f316eff4e50b0f8369ddfd4f6cc04f1e87feb
nateshao <shaotongjie@gmail.com>
2021-09-24 20:54:08 +0800

    Spring-ioc

--- 003-spring-injection/src/main/java/com/nateshao/App.java
+++ 003-spring-injection/src/main/java/com/nateshao/App.java
@@ -2,12 +2,17 @@ package com.nateshao;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j == i) {
+                    System.out.println(j + " * " + i + "=" + (j * i) + " ");
+                } else {
+                    System.out.print(j + " * " + i + "=" + (j * i) + " ");
+                }
+            }
+        }
     }
 }
