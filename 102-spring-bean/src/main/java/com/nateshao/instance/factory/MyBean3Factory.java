1c37ba650ce6268f77da5ee41260e0ffc2da2125
nateshao <shaotongjie@gmail.com>
2021-10-13 23:23:57 +0800

    ssm

--- 102-spring-bean/src/main/java/com/nateshao/instance/factory/MyBean3Factory.java
+++ 102-spring-bean/src/main/java/com/nateshao/instance/factory/MyBean3Factory.java
@@ -2,7 +2,7 @@ package com.nateshao.instance.factory;
 
 /**
  * @date Created by 邵桐杰 on 2021/10/13 23:00
- * @微信公众号 千羽的编程时光
+ * @微信公众号 程序员千羽
  * @个人网站 www.nateshao.cn
  * @博客 https://nateshao.gitee.io
  * @GitHub https://github.com/nateshao
