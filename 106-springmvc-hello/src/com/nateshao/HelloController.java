77af48caf73209933db17fb382a2d446f0a86727
nateshao <shaotongjie@gmail.com>
2021-10-16 16:21:53 +0800

    springmvc

--- 106-springmvc-hello/src/com/nateshao/HelloController.java
+++ 106-springmvc-hello/src/com/nateshao/HelloController.java
@@ -1,10 +1,10 @@
-package com.nateshao.controller;
+package com.nateshao;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * @date Created by 邵桐杰 on 2021/10/15 9:13
+ * @date Created by 邵桐杰 on 2021/10/16 16:12
  * @微信公众号 程序员千羽
  * @个人网站 www.nateshao.cn
  * @博客 https://nateshao.gitee.io
@@ -15,8 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("hello")
     public String hello(){
-        return "hello by";
+        return "hello";
     }
+
 }
