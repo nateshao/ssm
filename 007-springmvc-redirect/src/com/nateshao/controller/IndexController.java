132c9c28b765995969aad5d23233d8cabc0d4ae5
nateshao <shaotongjie@gmail.com>
2021-10-21 19:24:56 +0800

    a

---
+++ 007-springmvc-redirect/src/com/nateshao/controller/IndexController.java
@@ -0,0 +1,29 @@
+package com.nateshao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @date Created by 邵桐杰 on 2021/10/21 19:13
+ * @微信公众号 程序员千羽
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/isLogin")
+    public String login(){
+        return "forword:/isLogin";
+    }
+
+    @RequestMapping("/isRegister")
+    public String isRegister(){
+        return "redirect:/isRegister";
+    }
+
+
+}
