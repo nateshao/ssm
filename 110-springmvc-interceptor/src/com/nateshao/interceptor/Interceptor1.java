6ee57a6b098925493d070b69eb98e38a697c4f26
nateshao <shaotongjie@gmail.com>
2021-10-22 12:53:11 +0800

    ssm

---
+++ 110-springmvc-interceptor/src/com/nateshao/interceptor/Interceptor1.java
@@ -0,0 +1,37 @@
+package com.nateshao.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @date Created by 邵桐杰 on 2021/10/22 12:51
+ * @微信公众号 程序员千羽
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Interceptor1 implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("Intercepter1...preHandle");
+        return true;
+    }
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        System.out.println("Intercepter1...postHandle");
+    }
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response, Object handler,
+                                Exception ex) throws Exception {
+        System.out.println("Intercepter1...afterCompletion");
+    }
+}
---
+++ 110-springmvc-interceptor/src/com/nateshao/po/User.java
@@ -0,0 +1,49 @@
+package com.nateshao.po;
+
+/**
+ * @date Created by 邵桐杰 on 2021/10/22 12:47
+ * @微信公众号 程序员千羽
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class User {
+    private Integer id;        //id
+    private String username;  //用户名
+    private String password;  //密码
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
